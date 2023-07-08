9042186b3816140588a1e7e154740b685ed22f1e
fabba1892 <fabian.vanwyk@gmail.com>
2023-07-09 00:16:34 +0200

    first commit

---
+++ apis.py
@@ -0,0 +1,38 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+messages = client.messages.list(limit=20)
+
+for record in messages:
+    print(record.sid)
+
+
+# # Download the helper library from https://www.twilio.com/docs/python/install
+# from datetime import datetime
+# import os
+# from twilio.rest import Client
+
+
+# # Find your Account SID and Auth Token at twilio.com/console
+# # and set the environment variables. See http://twil.io/secure
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# client = Client(account_sid, auth_token)
+
+# messages = client.messages.list(
+#                                date_sent=datetime(2016, 8, 31, 0, 0, 0),
+#                                from_='+15017122661',
+#                                to='+15558675310',
+#                                limit=20
+#                            )
+
+# for record in messages:
+#     print(record.sid)
